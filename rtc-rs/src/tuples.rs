5c5641a1237a0408e639dc7167bf2514754b13e1
Fabio Falci <fabiofalci@gmail.com>
2021-01-01 22:18:20 +0000

    Remove tuples trait and adding more tests

--- rtc-rs/src/tuples.rs
+++ rtc-rs/src/tuples.rs
@@ -2,21 +2,8 @@ use std::ops::{Add, Sub, Neg, Mul, Div};
 
 struct Tuple(f32, f32, f32, f32);
 
-trait Tuples {
-    fn new(x: f32, y: f32, z: f32, w: f32) -> Self;
-    fn point(x: f32, y: f32, z: f32) -> Self;
-    fn vector(x: f32, y: f32, z: f32) -> Self;
-
-    fn x(&self) -> f32;
-    fn y(&self) -> f32;
-    fn z(&self) -> f32;
-    fn w(&self) -> f32;
-
-    fn is_point(&self) -> bool;
-    fn is_vector(&self) -> bool;
-}
-
-impl Tuples for Tuple {
+#[allow(dead_code)]
+impl Tuple {
     fn new(x: f32, y: f32, z: f32, w: f32) -> Tuple {
         Tuple(x, y, z, w)
     }
@@ -52,13 +39,17 @@ impl Tuples for Tuple {
     fn is_vector(&self) -> bool {
         self.w() == 0.0
     }
+
+    fn magnitude(&self) -> f32 {
+        (self.x().powi(2) + self.y().powi(2) + self.z().powi(2) + self.w().powi(2)).sqrt()
+    }
 }
 
 impl Add for Tuple {
     type Output = Tuple;
 
     fn add(self, rhs: Self) -> Self::Output {
-        Tuples::new(self.x() + rhs.x(), self.y() + rhs.y(), self.z() + rhs.z(), self.w() + rhs.w())
+        Tuple::new(self.x() + rhs.x(), self.y() + rhs.y(), self.z() + rhs.z(), self.w() + rhs.w())
     }
 }
 
@@ -66,7 +57,7 @@ impl Sub for Tuple {
     type Output = Tuple;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        Tuples::new(self.x() - rhs.x(), self.y() - rhs.y(), self.z() - rhs.z(), self.w() - rhs.w())
+        Tuple::new(self.x() - rhs.x(), self.y() - rhs.y(), self.z() - rhs.z(), self.w() - rhs.w())
     }
 }
 
@@ -74,7 +65,7 @@ impl Neg for Tuple {
     type Output = Tuple;
 
     fn neg(self) -> Self::Output {
-        Tuples::new(-self.x(), -self.y(), -self.z(), -self.w())
+        Tuple::new(-self.x(), -self.y(), -self.z(), -self.w())
     }
 }
 
@@ -82,7 +73,7 @@ impl Mul<f32> for Tuple {
     type Output = Tuple;
 
     fn mul(self, rhs: f32) -> Self::Output {
-        Tuples::new(self.x() * rhs, self.y() * rhs, self.z() * rhs, self.w() * rhs)
+        Tuple::new(self.x() * rhs, self.y() * rhs, self.z() * rhs, self.w() * rhs)
     }
 }
 
@@ -90,17 +81,17 @@ impl Div<f32> for Tuple {
     type Output = Tuple;
 
     fn div(self, rhs: f32) -> Self::Output {
-        Tuples::new(self.x() / rhs, self.y() / rhs, self.z() / rhs, self.w() / rhs)
+        Tuple::new(self.x() / rhs, self.y() / rhs, self.z() / rhs, self.w() / rhs)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::tuples::{Tuple, Tuples};
+    use crate::tuples::{Tuple};
 
     #[test]
     fn tuple_with_1_is_a_point() {
-        let tuple: Tuple = Tuples::new(4.3, -4.2, 3.1, 1.0);
+        let tuple = Tuple::new(4.3, -4.2, 3.1, 1.0);
 
         assert_eq!(tuple.x(), 4.3);
         assert_eq!(tuple.y(), -4.2);
@@ -112,7 +103,7 @@ mod tests {
 
     #[test]
     fn tuple_with_0_is_a_vector() {
-        let tuple: Tuple = Tuples::new(4.3, -4.2, 3.1, 0.0);
+        let tuple = Tuple::new(4.3, -4.2, 3.1, 0.0);
 
         assert_eq!(tuple.x(), 4.3);
         assert_eq!(tuple.y(), -4.2);
@@ -124,7 +115,7 @@ mod tests {
 
     #[test]
     fn create_point() {
-        let tuple: Tuple = Tuples::point(4.3, -4.2, 3.1);
+        let tuple = Tuple::point(4.3, -4.2, 3.1);
 
         assert_eq!(tuple.w(), 1.0);
         assert_eq!(tuple.is_point(), true);
@@ -133,7 +124,7 @@ mod tests {
 
     #[test]
     fn create_vector() {
-        let tuple: Tuple = Tuples::vector(4.3, -4.2, 3.1);
+        let tuple = Tuple::vector(4.3, -4.2, 3.1);
 
         assert_eq!(tuple.w(), 0.0);
         assert_eq!(tuple.is_point(), false);
@@ -142,8 +133,8 @@ mod tests {
 
     #[test]
     fn adding_two_tuples() {
-        let a1: Tuple = Tuples::new(3.0, -2.0, 5.0, 1.0);
-        let a2: Tuple = Tuples::new(-2.0, 3.0, 1.0, 0.0);
+        let a1 = Tuple::new(3.0, -2.0, 5.0, 1.0);
+        let a2 = Tuple::new(-2.0, 3.0, 1.0, 0.0);
 
         let a3 = a1 + a2;
 
@@ -155,8 +146,8 @@ mod tests {
 
     #[test]
     fn subtracting_two_points() {
-        let p1: Tuple = Tuples::point(3.0, 2.0, 1.0);
-        let p2: Tuple = Tuples::point(5.0, 6.0, 7.0);
+        let p1 = Tuple::point(3.0, 2.0, 1.0);
+        let p2 = Tuple::point(5.0, 6.0, 7.0);
 
         let vector = p1 - p2;
 
@@ -168,8 +159,8 @@ mod tests {
 
     #[test]
     fn subtracting_vector_from_points() {
-        let p1: Tuple = Tuples::point(3.0, 2.0, 1.0);
-        let v1: Tuple = Tuples::vector(5.0, 6.0, 7.0);
+        let p1 = Tuple::point(3.0, 2.0, 1.0);
+        let v1 = Tuple::vector(5.0, 6.0, 7.0);
 
         let point = p1 - v1;
 
@@ -181,8 +172,8 @@ mod tests {
 
     #[test]
     fn subtracting_two_vectors() {
-        let v1: Tuple = Tuples::vector(3.0, 2.0, 1.0);
-        let v2: Tuple = Tuples::vector(5.0, 6.0, 7.0);
+        let v1 = Tuple::vector(3.0, 2.0, 1.0);
+        let v2 = Tuple::vector(5.0, 6.0, 7.0);
 
         let vector = v1 - v2;
 
@@ -194,8 +185,8 @@ mod tests {
 
     #[test]
     fn subtracting_vector_from_vector_zero() {
-        let zero: Tuple = Tuples::vector(0.0, 0.0, 0.0);
-        let vector: Tuple = Tuples::vector(1.0, -2.0, 3.0);
+        let zero = Tuple::vector(0.0, 0.0, 0.0);
+        let vector = Tuple::vector(1.0, -2.0, 3.0);
 
         let result = zero - vector;
 
@@ -207,7 +198,7 @@ mod tests {
 
     #[test]
     fn negating_a_tuple() {
-        let tuple: Tuple = Tuples::new(1.0, -2.0, 3.0, -4.0);
+        let tuple = Tuple::new(1.0, -2.0, 3.0, -4.0);
 
         let result = -tuple;
 
@@ -219,7 +210,7 @@ mod tests {
 
     #[test]
     fn multiply_tuple_by_scalar() {
-        let tuple: Tuple = Tuples::new(1.0, -2.0, 3.0, -4.0);
+        let tuple = Tuple::new(1.0, -2.0, 3.0, -4.0);
 
         let result = tuple * 3.5;
 
@@ -231,7 +222,7 @@ mod tests {
 
     #[test]
     fn multiply_tuple_by_fraction() {
-        let tuple: Tuple = Tuples::new(1.0, -2.0, 3.0, -4.0);
+        let tuple = Tuple::new(1.0, -2.0, 3.0, -4.0);
 
         let result = tuple * 0.5;
 
@@ -243,7 +234,7 @@ mod tests {
 
     #[test]
     fn diving_tuple_by_scalar() {
-        let tuple: Tuple = Tuples::new(1.0, -2.0, 3.0, -4.0);
+        let tuple = Tuple::new(1.0, -2.0, 3.0, -4.0);
 
         let result = tuple / 2.0;
 
@@ -251,6 +242,15 @@ mod tests {
         assert_eq!(result.y(), -1.0);
         assert_eq!(result.z(), 1.5);
         assert_eq!(result.w(), -2.0);
+    }
+
+    #[test]
+    fn computing_vectors_magnitude() {
+        assert_eq!(Tuple::vector(1.0, 0.0, 0.0).magnitude(), 1.0);
+        assert_eq!(Tuple::vector(0.0, 1.0, 0.0).magnitude(), 1.0);
+        assert_eq!(Tuple::vector(0.0, 0.0, 1.0).magnitude(), 1.0);
+        assert_eq!(Tuple::vector(1.0, 2.0, 3.0).magnitude(), 14.0_f32.sqrt());
+        assert_eq!(Tuple::vector(-1.0, -2.0, -3.0).magnitude(), 14.0_f32.sqrt());
     }
 
 }
