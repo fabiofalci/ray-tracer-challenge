c0436e7b04c93adaf19237b760935d191fcb91e6
Fabio Falci <fabiofalci@gmail.com>
2021-01-27 18:20:53 +0000

    Modifying previous challenge with shadding

--- rtc-kt/src/test/kotlin/raytracer/Chapter05.kt
+++ rtc-kt/src/test/kotlin/raytracer/Chapter05.kt
@@ -6,29 +6,6 @@ import java.io.File
 internal class Chapter05Test {
 
     @Test
-    fun `Cast rays at a sphere`() {
-        val canvas = Canvas(500, 500)
-        val sphere = Sphere()
-
-        val translation = Transformations.translation(250.0, 250.0, 10.0)
-        val scaling = Transformations.scaling(230.0, 230.0, 5.0)
-        sphere.transform = Matrices.multiply(translation, scaling)
-
-        for (x in 0..canvas.width) {
-            for (y in 0..canvas.height) {
-                val ray = Ray(Tuple.point(x.toDouble(), y.toDouble(), -20.0), Tuple.vector(0.0, 0.0, 2.0))
-                val intersections = sphere.intersect(ray)
-                val hit = intersections.hit()
-                if (hit != null) {
-                    canvas.writePixel(x, y, Colors.RED)
-                }
-            }
-        }
-
-        File("/tmp/canvas.ppm").writeText(canvas.toPPM())
-    }
-
-    @Test
     fun `Cast rays at a sphere book suggestion`() {
         val rayOrigin = Tuple.point(0.0, 0.0, -5.0)
         val wallZ = 10.0
@@ -63,6 +40,59 @@ internal class Chapter05Test {
                         colorIndex = 0;
                     }
                     canvas.writePixel(x, y, colors[colorIndex])
+                } else {
+                    canvas.writePixel(x, y, colors[getBackgroundColor(worldX, worldY)])
+                }
+                println("y=$y x=$x = point $position = intersections $intersections = hit $hit")
+            }
+        }
+        File("/tmp/canvas.ppm").writeText(canvas.toPPM())
+    }
+
+
+    @Test
+    fun `Cast rays at a sphere book suggestion with shading`() {
+        val rayOrigin = Tuple.point(0.0, 0.0, -5.0)
+        val wallZ = 10.0
+        val wallSize = 7.0
+        val canvasPixels = 100
+        val pixelSize = wallSize / canvasPixels
+        val half = wallSize / 2
+        println("Pixel size is $pixelSize, half is $half")
+
+        val canvas = Canvas(canvasPixels, canvasPixels)
+        val shape = Sphere()
+        shape.material = Material()
+        shape.material.color = Tuple.color(1.0, 0.2, 1.0)
+        val lightPosition = Tuple.point(-10.0, 10.0, -10.0)
+        val lightColor = Colors.WHITE
+        val light = PointLight(lightPosition, lightColor)
+
+
+//        shape.transform = Transformations.scaling(1.0, 0.5, 1.0)
+//        shape.transform = Matrices.multiply(Transformations.rotationZ(PI / 4), Transformations.scaling(0.5, 1.0, 1.0))
+//        shape.transform = Matrices.multiply(Transformations.shearing(1.0, 0.0, 0.0, 0.0, 0.0, 0.0), Transformations.scaling(0.5, 1.0, 1.0))
+
+//        val colors = listOf(Colors.RED, Colors.GREEN, Colors.BLUE)
+        val colors = listOf(Colors.WHITE, Colors.RED, Colors.GREEN, Colors.BLACK)
+
+        for (y in 0 until canvasPixels) {
+            val worldY = half - pixelSize * y
+
+            for (x in 0 until canvasPixels) {
+                val worldX = -half + pixelSize * x
+                val position = Tuple.point(worldX, worldY, wallZ)
+                val ray = Ray(rayOrigin, (position - rayOrigin).normalize())
+                val intersections = shape.intersect(ray)
+
+                val hit = intersections.hit();
+                if (hit != null) {
+                    val point = ray.position(hit.time)
+                    val normal = hit.obj.normalAt(point)
+                    val eye = -ray.direction
+                    val color = Material.lighting(hit.obj.material, light, point, eye, normal)
+
+                    canvas.writePixel(x, y, color)
                 } else {
                     canvas.writePixel(x, y, colors[getBackgroundColor(worldX, worldY)])
                 }
